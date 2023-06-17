6d30a05ccb4c3ccaf89d9c4abca6da8376565e9f
Subhra Sankha Saha <jeesan2006@gmail.com>
2023-06-17 15:41:32 +0530

    Add project files.

---
+++ Data/PlatformRepo.cs
@@ -0,0 +1,52 @@
+﻿using PlatformService.Models;
+using System.Linq;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if(platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _context.Platforms.Add(platform);
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            if (id > 0)
+            {
+                return _context.Platforms.FirstOrDefault(p => p.Id == id);
+
+            }
+
+            return (Platform)Enumerable.Empty<Platform>();
+        }
+
+            public IEnumerable<Platform> GetPlatforms()
+        { 
+            return _context.Platforms.ToList();
+            
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public void UpdatePlatform(Platform platform)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/PrepDb.cs
@@ -0,0 +1,37 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder application)
+        {
+            using (var serviceScope = application.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                if (context != null)
+                {
+                    SeedData(context);
+                }
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Seeding Data.");
+
+                context.Platforms.AddRange(
+                        new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                        new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                        new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                        );
+            }
+            else
+            {
+                Console.WriteLine("We already have data.");
+            }
+        }
+    }
+}
