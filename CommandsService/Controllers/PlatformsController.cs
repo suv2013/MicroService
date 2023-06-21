f2427d13599134e1c79fb95bad4b3584cbf736e3
Subhra Sankha Saha <jeesan2006@gmail.com>
2023-06-21 17:00:35 +0530

    Updated Solution Level

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controler");
+        }
+    }
+}
